386c08319cbede9c0bd4ce7390ff500efcd7c40f
RenlyH <renly.hou@gmail.com>
2022-10-28 13:41:03 -0400

    adding opensrh

---
+++ datasets.py
@@ -0,0 +1,48 @@
+
+#def build_opensrh(cf='opensrh_data.yaml'):
+#    import yaml 
+#    from opensrh.datasets.srh_dataset import SRHClassificationDataset
+#    from opensrh.datasets.improc import get_srh_aug_list
+#    cf = yaml.load(open(cf), Loader=yaml.FullLoader)
+#    train_dset = SRHClassificationDataset(
+#        data_root=cf["data"]["db_root"],
+#        studies="train",
+#        transform=Compose(
+#            get_srh_aug_list(cf["data"]["train_augmentation"],
+#                             cf["data"]["rand_aug_prob"])),
+#        balance_patch_per_class=cf["data"]["balance_patch_per_class"])
+#    val_dset = SRHClassificationDataset(
+#        data_root=cf["data"]["db_root"],
+#        studies="val",
+#        transform=Compose(
+#            get_srh_aug_list(cf["data"]["valid_augmentation"],
+#                             cf["data"]["rand_aug_prob"])),
+#        balance_patch_per_class=False)
+#    return train_dset, val_dset
+
+def build_opensrh(cf='../opensrh_data.yaml'):
+    import yaml
+    from torchvision.transforms import Compose
+    from opensrh.datasets.srh_dataset import SRHClassificationDataset, SRHContrastiveDataset
+    from opensrh.datasets.improc import get_srh_aug_list
+    cf = yaml.load(open(cf), Loader=yaml.FullLoader)
+    train_dset = SRHContrastiveDataset(
+        data_root=cf["data"]["db_root"],
+        studies="train",
+        transform=Compose(
+            get_srh_aug_list(cf["data"]["train_augmentation"],
+                             cf["data"]["rand_aug_prob"])),
+        balance_patch_per_class=cf["data"]["balance_patch_per_class"])
+    val_dset = SRHContrastiveDataset(
+        data_root=cf["data"]["db_root"],
+        studies="val",
+        transform=Compose(
+            get_srh_aug_list(cf["data"]["valid_augmentation"],
+                             cf["data"]["rand_aug_prob"])),
+        balance_patch_per_class=False)
+    return train_dset, val_dset
+
+if __name__ == "__main__":
+    train_dataset, valid_dataset = build_opensrh("../opensrh_data.yaml")
+    import pdb
+    pdb.set_trace()
---
+++ submit.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#SBATCH --job-name=mae_pretr
+#SBATCH --mail-type=END,FAIL
+#SBATCH --nodes=1
+#SBATCH --gres=gpu:8
+#SBATCH --time=2-00:00:00
+
+#SBATCH --ntasks-per-gpu=5
+#SBATCH --mem-per-cpu=16g
+#SBATCH --account=precisionhealth_owned1
+#SBATCH --partition=precisionhealth
+
+##SBATCH --account=precisionhealth_project1
+##SBATCH --partition=gpu
+##SBATCH --ntasks-per-gpu=4
+##SBATCH --mem-per-cpu=7g
+
+##SBATCH --exclude=armis[28000,28001,28004]
+
+#SBATCH --array=0-0
+#SBATCH --output=./slurm_out/%x-%A_%a.out
+source activate torchsrh
+
+python main_simsiam.py \
+  -a resnet50 \
+  --dist-url 'tcp://localhost:10001' --multiprocessing-distributed --world-size 1 --rank 0 \
+  --fix-pred-lr  
